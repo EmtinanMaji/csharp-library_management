75b86448d7bcd6c6bda5f3c037a30172106c227f
EmtinanMaji <emtinanmaji@gmail.com>
2024-04-16 09:27:27 +0300

    Basic Classes

--- src/Program.cs
+++ src/Program.cs
@@ -1,4 +1,39 @@
-﻿internal class Program
+﻿using System;
+
+class Book
+{
+    public int Id { get; } 
+    public string Title { get; } 
+    public DateTime CreatedDate { get; } 
+
+    public Book(int id,string title, DateTime createdDate)
+    {
+        Id = id; 
+        Title = title;
+        CreatedDate = createdDate;
+    }
+}
+
+class User
+{
+    public int Id { get; } 
+    public string Name { get; } 
+    public DateTime CreatedDate { get; } 
+    
+    public User(int id, string name, DateTime createdDate)
+    {
+        Id = id;
+        Name = name;
+        CreatedDate = createdDate;
+    }
+}
+
+class Library
+{
+    
+}
+
+internal class Program
 {
     private static void Main()
     {
